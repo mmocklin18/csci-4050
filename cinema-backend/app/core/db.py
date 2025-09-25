938c9ff3544f56b0e4b803effdebe5755ed144d9
Matt Mocklin <matt.mocklin@gmail.com>
2025-09-25 16:10:48 -0400

    Connect to backend, added healthcheck for db connection

---
+++ cinema-backend/app/core/db.py
@@ -0,0 +1,33 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+#Get connection strings from env
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_SSL_CA = os.getenv("DB_SSL_CA")
+
+#form db url
+DATABASE_URL = (
+    f"mysql+asyncmy://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    f"?ssl_ca={DB_SSL_CA}"
+)
+
+#Create the async engine that manages connection pool
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+
+#session factory used to create session objects
+async_session = sessionmaker(
+    engine, expire_on_commit=False, class_=AsyncSession
+)
+
+#dependency to start a new db session
+async def get_session():
+    async with async_session() as session:
+        yield session
