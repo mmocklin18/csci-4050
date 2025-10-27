7f1e02107248d6a920b0a5e69e3f3831f47193ce
Matt Mocklin <matt.mocklin@gmail.com>
2025-10-27 15:48:21 -0400

    Fix registration and login connection

--- cinema-backend/app/main.py
+++ cinema-backend/app/main.py
@@ -2,13 +2,27 @@ from fastapi import FastAPI
 from app.routers.health import router as health_router
 from app.routers.movies import router as movie_router
 from app.routers.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000", 
+        "http://127.0.0.1:3000"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(health_router)
 app.include_router(movie_router)
 app.include_router(auth_router)
+
 
 @app.get("/")
 def root():
