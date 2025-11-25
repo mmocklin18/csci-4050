a0f9128726f954114b7862a2ebbf86eb89ffbbdc
arjun <arjun.sa@live.com>
2025-11-24 20:04:43 -0500

    Revert "add price model, schema, route"
    This reverts commit 5c080371c7981ebddc0ac35451936bf2a391ac43.

--- cinema-backend/app/models/price.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, DECIMAL
-from app.core.db import Base
-
-class Price(Base):
-    __tablename__ = "prices"
-
-    prices_id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    type = Column(String(50), nullable=False)
-    amount = Column(DECIMAL(10, 2), nullable=False)
